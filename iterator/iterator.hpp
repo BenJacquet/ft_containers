5e7fff4a2c031156eff4b7cca6e1eab5d84fad04
Jacquet BENJAMIN <jabenjam@student.42.fr>
2022-04-11 16:59:45 +0200

    did not do anything

--- iterator/iterator.hpp
+++ iterator/iterator.hpp
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   vector.hpp                                         :+:      :+:    :+:   */
+/*   iterator.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jabenjam <jabenjam@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/04/07 12:24:50 by jabenjam          #+#    #+#             */
-/*   Updated: 2022/04/11 16:33:00 by jabenjam         ###   ########.fr       */
+/*   Created: 2022/04/11 16:00:08 by jabenjam          #+#    #+#             */
+/*   Updated: 2022/04/11 16:33:20 by jabenjam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,18 @@
 
 namespace ft
 {
+	template <typename _Category, typename _Tp, typename _Distance = ptrdiff_t,
+			  typename _Pointer = _Tp *, typename _Reference = _Tp &>
+	struct iterator
+	{
 
+	}
+
+	/// Partial specialization for pointer types.
+	template <typename _Tp>
+	struct iterator_traits<_Tp *>
+
+	/// Partial specialization for const pointer types.
+	template <typename _Tp>
+	struct iterator_traits<const _Tp *>
 };
